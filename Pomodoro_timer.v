9690a887307fb5cc14156fbd96db58cb460fb517
Amyra Nguyen <thanhhien.nguyen6603@gmail.com>
2022-05-03 15:52:44 +0700

    UPDATE afternoon

--- Pomodoro_timer.v
+++ Pomodoro_timer.v
@@ -31,99 +31,107 @@ module Pomodoro_timer(
     
     input clk;
     input rst;
-    input [3:0]btn;
+    input [3:0] btn;
     output sclk;
     output rclk;
     output dio;
     
-    reg [15:0] data;
-    reg [ 2:0] cnt;
-    reg [15:0] displayed_number = 'd0;
-    reg [15:0] set_time;
-    reg [15:0] mod_cnt;
-    reg [26:0] one_second_counter; 
-    wire one_second_enable;
-    wire       vld;
+    parameter       COUNT_LIM = 27'd125000000;
+    parameter       LED_WIDTH = 'd32;
+    parameter       DIG_WIDTH = 'd64;
     
-    reg [3:0] LED_0;
-    reg [3:0] LED_1;
-    reg [3:0] LED_2;
-    reg [3:0] LED_3;
-    reg [3:0] LED_4;
-    reg [3:0] LED_5;
-    reg [3:0] LED_6;
-    reg [3:0] LED_7;    
     
-    reg [3:0] NUM_0;
-    reg [3:0] NUM_1;
-    reg [3:0] NUM_2;
-    reg [3:0] NUM_3;
-    reg [3:0] NUM_4;
-    reg [3:0] NUM_5;
-    reg [3:0] NUM_6;
-    reg [3:0] NUM_7;
+                    
+    parameter       BTN3 = 4'b1000, BTN2 = 4'b0100,
+                    BTN1 = 4'b0010, BTN0 = 4'b0001;
+                    
+    reg [15:0]                  data;
+    reg [2:0]                   cnt;
+    reg [15:0]                  displayed_number = 'd0;
+    reg [15:0]                  set_time;
+    reg [15:0]                  mod_cnt;
+    reg [26:0]                  one_second_counter; 
+    wire                        one_second_enable;
+    wire                        vld;
+    
+//    reg [DIG_WIDTH - 1:0]       BCD;
+//    reg [LED_WIDTH - 1:0]       LED;
+    
+    wire            seconds;
+    reg [3:0]       LED_0;
+    reg [3:0]       LED_1;
+    reg [3:0]       LED_2;
+    reg [3:0]       LED_3;
+    reg [3:0]       LED_4;
+    reg [3:0]       LED_5;
+    reg [3:0]       LED_6;
+    reg [3:0]       LED_7;
+
+    wire [7:0]       NUM_0;
+    wire [7:0]       NUM_1;
+    wire [7:0]       NUM_2;
+    wire [7:0]       NUM_3;
+    wire [7:0]       NUM_4;
+    wire [7:0]       NUM_5;
+    wire [7:0]       NUM_6;
+    wire [7:0]       NUM_7;
+    
     always @(posedge clk or posedge rst)
         begin
-            if(rst==1)
+            if(rst)
                 one_second_counter <= 0;
             else begin
-                if(one_second_counter>=124999999) 
+                if(one_second_counter == COUNT_LIM - 1) 
                      one_second_counter <= 0;
                 else
                     one_second_counter <= one_second_counter + 1;
             end
         end 
-        
-        always @(*)
-        begin
-            case(btn)
-            4'b1000: begin set_time = 'd300;//5 minutes
-                           mod_cnt = 'd0;
-                     end
-            4'b0100: begin set_time = 'd600;//10 minutes
-                           mod_cnt = 'd0;
-                     end
-            4'b0010: begin set_time = 'd1500;//25 minutes
-                           mod_cnt = 'd0;
-                     end
-            4'b0001: begin set_time = 'd3000;//50 minutes
-                           mod_cnt = 'd0;
-                     end
-            endcase
+    
+    always @(btn)
+    begin
+        case(btn)
+        BTN3: begin set_time = 'd300;//5 minutes
+                       mod_cnt = 'd0;
+                 end
+        BTN2: begin set_time = 'd600;//10 minutes
+                       mod_cnt = 'd0;
+                 end
+        BTN1: begin set_time = 'd1500;//25 minutes
+                       mod_cnt = 'd0;
+                 end
+        BTN0: begin set_time = 'd3000;//50 minutes
+                       mod_cnt = 'd0;
+                 end
+        default: mod_cnt <= mod_cnt;
+        endcase
+    end
+    
+    assign one_second_enable = (one_second_counter == COUNT_LIM - 1) ? 1 : 0;
+    assign seconds = set_time-displayed_number;
+    
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin
+            displayed_number <= 0;
+            {LED_0, LED_1, LED_2, LED_3, LED_4, LED_5, LED_6, LED_7} = {LED_WIDTH{1'b0}};
         end
-     assign one_second_enable = (one_second_counter==124999999)?1:0;
-            always @(posedge clk or posedge rst)
-            begin
-                if(rst==1)
-                  begin
-                    displayed_number <= 0;
-                    LED_0 <= 'd0;
-                    LED_1 <= 'd0;
-                    LED_2 <= 'd0;
-                    LED_3 <= 'd0;
-                    LED_4 <= 'd0;
-                    LED_5 <= 'd0;
-                    LED_6 <= 'd0;
-                    LED_7 <= 'd0;
-                  end
-                else if(one_second_enable==1)
-                   begin
-                    displayed_number <= displayed_number + 1;
-                    LED_4 <= (set_time-displayed_number)/600;
-                    LED_5 <= ((set_time-displayed_number) % 600)/60;
-                    LED_6 <= (((set_time-displayed_number) % 600)%60)/10;
-                    LED_7 <= (((set_time-displayed_number) % 600)%60)%10;
-                    if (displayed_number == set_time)
-                      begin
-                        mod_cnt <= mod_cnt + 1;
-                        LED_0 <= (mod_cnt)/1000;
-                        LED_1 <= (mod_cnt % 1000)/100;
-                        LED_2 <= ((mod_cnt % 1000)%100)/10;
-                        LED_3 <= ((mod_cnt % 1000)%100)%10;                        
-                      end
-                    else displayed_number <= 0;
-                   end
-            end  
+        else if(one_second_enable) begin
+            displayed_number <= displayed_number + 1;
+            LED_4 <= seconds /600;
+            LED_5 <= (seconds % 600)/60;
+            LED_6 <= ((seconds % 600)%60)/10;
+            LED_7 <= ((seconds % 600)%60)%10;
+            if (displayed_number == set_time)
+              begin
+                mod_cnt <= mod_cnt + 1;
+                LED_0 <= (mod_cnt)/1000;
+                LED_1 <= (mod_cnt % 1000)/100;
+                LED_2 <= ((mod_cnt % 1000)%100)/10;
+                LED_3 <= ((mod_cnt % 1000)%100)%10;                        
+              end
+            else displayed_number <= 0;
+           end
+    end  
     always @(posedge clk) begin
         if (rst) cnt <= 0;
         else if (vld) begin
@@ -150,114 +158,16 @@ wire ready;
             endcase
         end
     end
+ 
+    BCD_7_Segment display_0(.BCD(LED_0), .segment(NUM_0));
+    BCD_7_Segment display_1(.BCD(LED_1), .segment(NUM_1));
+    BCD_7_Segment display_2(.BCD(LED_2), .segment(NUM_2));
+    BCD_7_Segment display_3(.BCD(LED_3), .segment(NUM_3));
+    BCD_7_Segment display_4(.BCD(LED_4), .segment(NUM_4));
+    BCD_7_Segment display_5(.BCD(LED_5), .segment(NUM_5));
+    BCD_7_Segment display_6(.BCD(LED_6), .segment(NUM_6));
+    BCD_7_Segment display_7(.BCD(LED_7), .segment(NUM_7));
     
- always @(*)
-    begin
-        case(LED_0)
-        4'b0000: NUM_0 = 7'b0000001; // "0"     
-        4'b0001: NUM_0 = 7'b1001111; // "1" 
-        4'b0010: NUM_0 = 7'b0010010; // "2" 
-        4'b0011: NUM_0 = 7'b0000110; // "3" 
-        4'b0100: NUM_0 = 7'b1001100; // "4" 
-        4'b0101: NUM_0 = 7'b0100100; // "5" 
-        4'b0110: NUM_0 = 7'b0100000; // "6" 
-        4'b0111: NUM_0 = 7'b0001111; // "7" 
-        4'b1000: NUM_0 = 7'b0000000; // "8"     
-        4'b1001: NUM_0 = 7'b0000100; // "9" 
-        default: NUM_0 = 7'b0000001; // "0"
-        endcase
-            case(LED_1)
-            4'b0000: NUM_1 = 7'b0000001; // "0"     
-            4'b0001: NUM_1 = 7'b1001111; // "1" 
-            4'b0010: NUM_1 = 7'b0010010; // "2" 
-            4'b0011: NUM_1 = 7'b0000110; // "3" 
-            4'b0100: NUM_1 = 7'b1001100; // "4" 
-            4'b0101: NUM_1 = 7'b0100100; // "5" 
-            4'b0110: NUM_1 = 7'b0100000; // "6" 
-            4'b0111: NUM_1 = 7'b0001111; // "7" 
-            4'b1000: NUM_1 = 7'b0000000; // "8"     
-            4'b1001: NUM_1 = 7'b0000100; // "9" 
-            default: NUM_1 = 7'b0000001; // "0"
-            endcase
-                case(LED_2)
-                4'b0000: NUM_2 = 7'b0000001; // "0"     
-                4'b0001: NUM_2 = 7'b1001111; // "1" 
-                4'b0010: NUM_2 = 7'b0010010; // "2" 
-                4'b0011: NUM_2 = 7'b0000110; // "3" 
-                4'b0100: NUM_2 = 7'b1001100; // "4" 
-                4'b0101: NUM_2 = 7'b0100100; // "5" 
-                4'b0110: NUM_2 = 7'b0100000; // "6" 
-                4'b0111: NUM_2 = 7'b0001111; // "7" 
-                4'b1000: NUM_2 = 7'b0000000; // "8"     
-                4'b1001: NUM_2 = 7'b0000100; // "9" 
-                default: NUM_2 = 7'b0000001; // "0"
-                endcase
-                    case(LED_3)
-                    4'b0000: NUM_3 = 7'b0000001; // "0"     
-                    4'b0001: NUM_3 = 7'b1001111; // "1" 
-                    4'b0010: NUM_3 = 7'b0010010; // "2" 
-                    4'b0011: NUM_3 = 7'b0000110; // "3" 
-                    4'b0100: NUM_3 = 7'b1001100; // "4" 
-                    4'b0101: NUM_3 = 7'b0100100; // "5" 
-                    4'b0110: NUM_3 = 7'b0100000; // "6" 
-                    4'b0111: NUM_3 = 7'b0001111; // "7" 
-                    4'b1000: NUM_3 = 7'b0000000; // "8"     
-                    4'b1001: NUM_3 = 7'b0000100; // "9" 
-                    default: NUM_3 = 7'b0000001; // "0"
-                    endcase
-                        case(LED_4)
-                        4'b0000: NUM_4 = 7'b0000001; // "0"     
-                        4'b0001: NUM_4 = 7'b1001111; // "1" 
-                        4'b0010: NUM_4 = 7'b0010010; // "2" 
-                        4'b0011: NUM_4 = 7'b0000110; // "3" 
-                        4'b0100: NUM_4 = 7'b1001100; // "4" 
-                        4'b0101: NUM_4 = 7'b0100100; // "5" 
-                        4'b0110: NUM_4 = 7'b0100000; // "6" 
-                        4'b0111: NUM_4 = 7'b0001111; // "7" 
-                        4'b1000: NUM_4 = 7'b0000000; // "8"     
-                        4'b1001: NUM_4 = 7'b0000100; // "9" 
-                        default: NUM_4 = 7'b0000001; // "0"
-                        endcase
-                            case(LED_5)
-                            4'b0000: NUM_5 = 7'b0000001; // "0"     
-                            4'b0001: NUM_5 = 7'b1001111; // "1" 
-                            4'b0010: NUM_5 = 7'b0010010; // "2" 
-                            4'b0011: NUM_5 = 7'b0000110; // "3" 
-                            4'b0100: NUM_5 = 7'b1001100; // "4" 
-                            4'b0101: NUM_5 = 7'b0100100; // "5" 
-                            4'b0110: NUM_5 = 7'b0100000; // "6" 
-                            4'b0111: NUM_5 = 7'b0001111; // "7" 
-                            4'b1000: NUM_5 = 7'b0000000; // "8"     
-                            4'b1001: NUM_5 = 7'b0000100; // "9" 
-                            default: NUM_5 = 7'b0000001; // "0"
-                            endcase
-                                case(LED_6)
-                                4'b0000: NUM_6 = 7'b0000001; // "0"     
-                                4'b0001: NUM_6 = 7'b1001111; // "1" 
-                                4'b0010: NUM_6 = 7'b0010010; // "2" 
-                                4'b0011: NUM_6 = 7'b0000110; // "3" 
-                                4'b0100: NUM_6 = 7'b1001100; // "4" 
-                                4'b0101: NUM_6 = 7'b0100100; // "5" 
-                                4'b0110: NUM_6 = 7'b0100000; // "6" 
-                                4'b0111: NUM_6 = 7'b0001111; // "7" 
-                                4'b1000: NUM_6 = 7'b0000000; // "8"     
-                                4'b1001: NUM_6 = 7'b0000100; // "9" 
-                                default: NUM_6 = 7'b0000001; // "0"
-                                endcase
-                                    case(LED_7)
-                                    4'b0000: NUM_7 = 7'b0000001; // "0"     
-                                    4'b0001: NUM_7 = 7'b1001111; // "1" 
-                                    4'b0010: NUM_7 = 7'b0010010; // "2" 
-                                    4'b0011: NUM_7 = 7'b0000110; // "3" 
-                                    4'b0100: NUM_7 = 7'b1001100; // "4" 
-                                    4'b0101: NUM_7 = 7'b0100100; // "5" 
-                                    4'b0110: NUM_7 = 7'b0100000; // "6" 
-                                    4'b0111: NUM_7 = 7'b0001111; // "7" 
-                                    4'b1000: NUM_7 = 7'b0000000; // "8"     
-                                    4'b1001: NUM_7 = 7'b0000100; // "9" 
-                                    default: NUM_7 = 7'b0000001; // "0"
-                                    endcase
-   end
     mfe_led7seg_74hc595_controller led7seg_ctrl(
         .clk    (clk),
         .rst    (rst),
@@ -269,5 +179,20 @@ wire ready;
         .rclk   (rclk),
         .dio    (dio)
         );
-    
+//    mfe_led7seg_74hc595_wrapper
+//    #(
+//    .DIG_NUM    (DIG_NUM),
+//    .SEG_NUM    (SEG_NUM),
+//    .DIV_WIDTH  (DIV_WIDTH)
+//    )
+//led7seg_ctrl_wrapper(
+//    .clk        (clk),
+//    .rst        (rst),
+//    .dat        (data),
+//    .vld        (vld),
+
+//    .sclk       (sclk),
+//    .rclk       (rclk),
+//    .dio        (dio)
+//    );
 endmodule
--- Pomodoro_timer_tb.v
+++ Pomodoro_timer_tb.v
@@ -1,31 +1,33 @@
 `timescale 1ns / 1ps
-module Pomodoro_timer_tb (
-        clk,
-    rst,
-    btn,
-    sclk,
-    rclk,
-    dio
-);
+module Pomodoro_timer_tb ();
     reg clk;
     reg rst;
     reg [3:0]btn;
     wire sclk;
     wire rclk;
     wire dio;
-
-Pomodoro_timer Timer(clk, rst, btn, sclk, rclk, dio);
-initial begin 
-    $monitor("%0d", $time, clk, rst, btn, sclk, rclk, dio);
-    end
+    
+    //Clock Generator
     initial begin
-
-#10 btn = 4'b1000;
-
-#50 rst = 1;
-
-#60 btn = 4'b0001;
-
-#100 rst = 1;
-end
+        clk = 1'b0;
+        forever #0.5 clk = ~clk;
+    end
+    Pomodoro_timer Timer(clk, rst, btn, sclk, rclk, dio);
+    defparam Timer.COUNT_LIM = 'd1;
+    
+    initial begin
+        //Initial reset
+            rst = 0;
+        #2  rst = 1;
+        //Posedge-trigger
+        #2 btn = 4'b1000;
+        #1  btn = 4'b0000;
+        #400 rst = 1;
+        
+        #2 btn = 4'b0001;
+        #2 btn = 4'b0000;
+        #100 rst = 1;
+        #510 $stop;
+    end
+    initial #510 $finish;
 endmodule
